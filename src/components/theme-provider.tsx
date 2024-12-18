d7b95cf7991316b9a511b3dcdf34e4b92dba86e5
gunnerx64 <mr.sergey.sviridov@gmail.com>
2024-12-18 23:24:24 +0300

    fixed (almost all) Hydration issues

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,14 @@
 "use client";
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) return null;
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
